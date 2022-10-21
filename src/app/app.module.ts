3d01bb8c694048be1320cbe0f2604b811580cb1d
Exaxis <mwinstead49@gmail.com>
2022-10-21 12:39:36 -0400

    - Basic styling and landing page structure put in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { CrewComponent } from './crew/crew.component';
+
+const appRoutes: Routes = [
+  { path: '',   redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingComponent },
+  { path: 'crew', component: CrewComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CrewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
